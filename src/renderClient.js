e5a25a261e5cb71f6d4b09674341499ced820e34
Jeffrey Smith <js62789@gmail.com>
2018-04-18 21:26:53 -0400

    Add CSS dev support

--- src/renderClient.js
+++ src/renderClient.js
@@ -1,9 +1,11 @@
+import styles from './styles/main.css';
+
 export default async function renderClient() {
   const response = await fetch('/health');
   const { healthy: isHealthy } = await response.json();
 
   const html = `
-    <center>
+    <center class="${styles.healthStatus}">
       <h1>React Application</h1>
       <h2>Your application is ${isHealthy ? '' : 'not'} healthy</h2>
     </center>
